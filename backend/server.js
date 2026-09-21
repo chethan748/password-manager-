2aa1d3c806390eeb881263b8b4fa8f7361fe38fb
chethan <chethanm854@gmail.com>
2026-09-21 19:20:24 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -49,6 +49,6 @@ app.get("/verify",middleware, async (req, res) => {
     });
 });
 
-app.listen(port, () => {
+app.listen(port,"0.0.0.0", () => {
   console.log(` Server running: http://localhost:${port}`);
 });
